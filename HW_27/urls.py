fb7878f55b24eb5c22e29ef8e3a87ad66b3ad986
@omi-coder <omi-ufa@yandex.ru>
2022-11-02 21:02:45 +0300

    Done

--- HW_27/urls.py
+++ HW_27/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import path as path
+# import path as path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
