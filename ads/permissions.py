bca8116a5d14e979cd23fb65be6901dc253b95da
@omi-coder <omi-ufa@yandex.ru>
2022-11-13 10:24:47 +0300

    Updated

--- ads/permissions.py
+++ ads/permissions.py
@@ -7,15 +7,11 @@ class IsOwnerSelection(BasePermission):
     message = "Вы не являетесь владельцем данной подборки"
 
     def has_object_permission(self, request, view, obj):
-        if request.user == obj.owner:
-            return True
-        return False
+        return request.user == obj.owner
 
 
 class IsOwnerAdOrStaff(BasePermission):
     message = "Вы не являетесь владельцем объявления или админом"
 
     def has_object_permission(self, request, view, obj):
-        if request.user == obj.author or request.user.role in [UserRoles.ADMIN, UserRoles.MODERATOR]:
-            return True
-        return False
+        return request.user == obj.author or request.user.role in [UserRoles.ADMIN, UserRoles.MODERATOR]
